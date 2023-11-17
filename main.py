0ac79a4e7df46950d5c2ce6005e00e85286d7169
krisga90 <krzysztof.meyer@gmail.com>
2023-11-17 21:36:10 +0100

    print out all positions

--- main.py
+++ main.py
@@ -11,7 +11,22 @@ model.predict(source="./data/im1.jpg", save=True, conf=0.5, save_txt=True)
 
 Image("runs/detect/predict/labels/im1.jpg")
 
-
 model2 = YOLO("yolov8m.pt")
-model2.predict(source='data/video1.avi',  save=True, conf=0.5, save_txt=False)
+results = model2.predict(source='data/video1.avi',  save=False, conf=0.5, save_txt=False)
+# print(a.boxes)
+
+lista_list = [[]]
+for det in results:
+
+    boxes = det.boxes
+    for box in boxes:
+        lista = []
+        for data in box.data[0]:
+            lista.append(data.item())
+        lista_list.append(lista)
+
+print(lista_list)
+
+
+
 
